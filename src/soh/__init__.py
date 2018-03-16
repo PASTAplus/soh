55babeb300aa92cefa413a77a37a71b10e7e8652
servilla <mark.servilla@gmail.com>
2018-03-16 13:38:48 -0600

    Add project package constructors

---
+++ src/soh/__init__.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""":Mod: __init__
+
+:Synopsis:
+
+:Author:
+    servilla
+
+:Created:
+    3/16/18
+"""
+
+import logging
+import os
+
+import daiquiri
+
+cwd = os.path.dirname(os.path.realpath(__file__))
+logfile = cwd + '/soh.log'
+daiquiri.setup(level=logging.INFO,
+               outputs=(daiquiri.output.File(logfile), 'stdout',))
+logger = daiquiri.getLogger('__init__.py: ' + __name__)
+
+
+def main():
+    return 0
+
+
+if __name__ == "__main__":
+    main()

faca7c067b548d9be9efee8f359c1bc3189fd25b
servilla <mark.servilla@gmail.com>
2018-03-16 13:40:15 -0600

    Add server class unit test harness

---
+++ tests/test_server.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""":Mod: test_server
+
+:Synopsis:
+
+:Author:
+    servilla
+  
+:Created:
+    3/16/18
+"""
+import logging
+import os
+import sys
+import unittest
+
+import daiquiri
+
+from soh.config import Config
+from soh.server.server import Server
+
+
+daiquiri.setup(level=logging.INFO)
+logger = daiquiri.getLogger('test_server.py: ' + __name__)
+
+
+sys.path.insert(0, os.path.abspath('../src'))
+
+host = 'gypsum.lternet.edu'
+
+
+class TestServer(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_test_server(self):
+        server = Server(host)
+        server.test_server()
+        status = server.status
+        self.assertEqual(status, Config.UP)
+
+
+
+if __name__ == '__main__':
+    unittest.main()

a025ed3d1af7db656a8f381ba9062d34e1438228
servilla <mark.servilla@gmail.com>
2018-03-16 13:39:55 -0600

    Add package setup

---
+++ src/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""":Mod: setup.py
+
+:Synopsis:
+
+:Author:
+    servilla
+
+:Created:
+    3/16/18
+"""
+import setuptools
+
+setuptools.setup(name='pastaplus.soh',
+                 version='0.1',
+                 description='PASTA+ and related services State of Health monitoring',
+                 author='PASTA+ project',
+                 url='https://github.com/PASTAplus/soh',
+                 license='Apache License, Version 2.0',
+                 packages=setuptools.find_packages(),
+                 include_package_data=True,
+                 exclude_package_data={
+                     '': ['settings.py, properties.py, config.py'],
+                 }, )
+
+
+def main():
+    return 0
+
+
+if __name__ == "__main__":
+    main()

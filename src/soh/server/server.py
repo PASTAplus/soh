2509474ba9926614f91f9a1c160be92219e0ec7b
servilla <mark.servilla@gmail.com>
2018-03-30 10:21:42 -0600

    Add new server instances for Tomcat, Solr, and LDAP

--- src/soh/server/server.py
+++ src/soh/server/server.py
@@ -14,8 +14,11 @@
 import daiquiri
 
 from soh.config import Config
-from soh.asserts import server
 from soh.asserts import jetty
+from soh.asserts import ldap
+from soh.asserts import server
+from soh.asserts import solr
+from soh.asserts import tomcat
 
 logger = daiquiri.getLogger('server.py: ' + __name__)
 
@@ -40,21 +43,81 @@ class Server(object):
         return server_is_down
 
 
-class PastaServer(Server):
+class JettyServer(Server):
     """
-    The PastaServer uniquely identifies services provided by the Gatekeeper
-    service as identified by the host name "pasta".
+    The JettyServer uniquely identifies services provided by the PASTA
+    Gatekeeper service as identified by the host name "pasta".
     """
 
     @staticmethod
     def check_server(host=None):
         status = Config.UP
-        if PastaServer.jetty_is_down(host=host):
+        if JettyServer.jetty_is_down(host=host):
             status = status | Config.assertions['JETTY_DOWN']
-            if PastaServer.server_is_down(host=host):
+            if JettyServer.server_is_down(host=host):
                 status = status | Config.assertions['SERVER_DOWN']
         return status
 
     @staticmethod
     def jetty_is_down(host=None):
         return jetty.is_down(host=host)
+
+
+class TomcatServer(Server):
+    """
+    The TomcatServer identifies services provided by the workhorse PASTA
+    services, such as the Data Package Manager and Audit Manager.
+    """
+
+    @staticmethod
+    def check_server(host=None):
+        status = Config.UP
+        if TomcatServer.tomcat_is_down(host=host):
+            status = status | Config.assertions['TOMCAT_DOWN']
+            if TomcatServer.server_is_down(host=host):
+                status = status | Config.assertions['SERVER_DOWN']
+        return status
+
+    @staticmethod
+    def tomcat_is_down(host=None):
+        return tomcat.is_down(host=host)
+
+
+class SolrServer(Server):
+    """
+    The SolrServer identifies services provided by the PASTA search engine
+    service, Solr.
+    """
+
+    @staticmethod
+    def check_server(host=None):
+        status = Config.UP
+        if SolrServer.solr_is_down(host=host):
+            status = status | Config.assertions['SOLR_DOWN']
+            if SolrServer.server_is_down(host=host):
+                status = status | Config.assertions['SERVER_DOWN']
+        return status
+
+    @staticmethod
+    def solr_is_down(host=None):
+        return solr.is_down(host=host)
+
+
+class LdapServer(Server):
+    """
+    The LdapServer identifies services provided by the PASTA authentication
+    service, LDAP.
+    """
+
+    @staticmethod
+    def check_server(host=None):
+        status = Config.UP
+        if LdapServer.ldap_is_down(host=host):
+            status = status | Config.assertions['LDAP_DOWN']
+            if LdapServer.server_is_down(host=host):
+                status = status | Config.assertions['SERVER_DOWN']
+        return status
+
+    @staticmethod
+    def ldap_is_down(host=None):
+        return ldap.is_down(host=host)

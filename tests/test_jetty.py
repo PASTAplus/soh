6d094f408bac97af6520ae8deab2495ff536a5ee
servilla <mark.servilla@gmail.com>
2018-03-30 10:22:02 -0600

    Add test for Jetty server

--- tests/test_jetty.py
+++ tests/test_jetty.py
@@ -17,8 +17,9 @@ import unittest
 
 import daiquiri
 
-from soh.config import Config
 from soh.asserts import jetty
+from soh.config import Config
+from soh.server.server import JettyServer
 
 logger = daiquiri.getLogger('test_jetty.py: ' + __name__)
 
@@ -38,6 +39,10 @@ class TestJetty(unittest.TestCase):
     def test_jetty_is_down(self):
         is_down = jetty.is_down(host=host)
         self.assertIsNot(is_down, True)
+
+    def test_Jetty(self):
+        status = JettyServer.check_server(host=host)
+        self.assertEqual(status, Config.UP)
 
 
 if __name__ == '__main__':

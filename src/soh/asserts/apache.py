6e3bb8e7dae6536428b98f80dd2e0c370c36bc3e
servilla <mark.servilla@gmail.com>
2018-04-12 19:52:03 -0600

    Assert down only for server error and higher (>= 500)

--- src/soh/asserts/apache.py
+++ src/soh/asserts/apache.py
@@ -21,8 +21,9 @@ def is_down(host=None):
     url = 'http://' + host
     assert_is_down = True
     try:
-        r = requests.get(url=url, timeout=5.0)
-        assert_is_down = r.status_code != requests.codes.ok
+        r = requests.get(url=url, allow_redirects=False, timeout=5.0)
+        # Assert down only for server error and higher
+        assert_is_down = r.status_code >= requests.codes.server_error
     except Exception as e:
         logger.error(e)
     return assert_is_down

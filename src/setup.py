631b1ce12ce8ba84b8edc8f325a9862f073f2779
servilla <mark.servilla@gmail.com>
2018-04-18 12:13:59 -0600

    Increase bug version from 0 to 1

--- src/setup.py
+++ src/setup.py
@@ -14,7 +14,7 @@
 import setuptools
 
 setuptools.setup(name='pastaplus.soh',
-                 version='0.6.0',
+                 version='0.6.1',
                  description='PASTA+ and related services State of Health monitoring',
                  author='PASTA+ project',
                  url='https://github.com/PASTAplus/soh',

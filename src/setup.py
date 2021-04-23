edfc13d9ea6903ae78f1b9545f0c6d099dab5e00
Mark Servilla <mark.servilla@gmail.com>
2021-04-23 17:18:30 -0600

    Bump version

--- src/setup.py
+++ src/setup.py
@@ -14,7 +14,7 @@
 import setuptools
 
 setuptools.setup(name='pastaplus.soh',
-                 version='2021.01.24',
+                 version='2021.04.23',
                  description='PASTA+ and related services State of Health monitoring',
                  author='PASTA+ project',
                  url='https://github.com/PASTAplus/soh',

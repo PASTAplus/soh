30647caaf5aad6239c8f23224f109040f535a463
Mark Servilla <mark.servilla@gmail.com>
2022-05-09 20:40:21 -0600

    Add mimemail unit test

---
+++ tests/test_email.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+:Mod: test_email
+
+:Synopsis:
+
+:Author:
+    servilla
+
+:Created:
+    5/9/22
+"""
+import pytest
+
+import soh.mimemail
+
+
+def test_mailout():
+    subject = "Test"
+    message = "Testing email"
+
+    assert(soh.mimemail.send_mail(subject, message) is True)

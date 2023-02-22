d28a4c9a7dcf0eac9ea74d72879057e7aa226a93
James Brunt <12550652+jwbrunt@users.noreply.github.com>
2023-02-22 14:05:58 -0700

    Bump version date

--- src/setup.py
+++ src/setup.py
@@ -14,7 +14,7 @@
 import setuptools
 
 setuptools.setup(name='pastaplus.soh',
-                 version='2022.05.07',
+                 version='2023.02.22',
                  description='PASTA+ and related services State of Health monitoring',
                  author='PASTA+ project',
                  url='https://github.com/PASTAplus/soh',
